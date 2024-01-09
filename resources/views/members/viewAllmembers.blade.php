961403d92af0a9c451c5e18ca4e637725e2ae1de
khdream <truedev1001@gmail.com>
2024-01-09 16:35:30 +0900

    updated: new label for the members

--- resources/views/members/viewAllmembers.blade.php
+++ resources/views/members/viewAllmembers.blade.php
@@ -3,37 +3,40 @@
 @section('content')
 <div class="container-full mx-5">
     <div class="row justify-content-center">
+        <div>
+            <h3>会員一覧</h3>
+        </div>
         <table class="table table-hover text-center">
             <thead class="table-dark">
                 <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">法人名</th>
-                    <th scope="col">担当者氏名</th>
-                    <th scope="col">担当者ふりがな</th>
-                    <th scope="col">メールアドレス</th>
-                    <th scope="col">電話番号</th>
-                    <th scope="col">郵便番号</th>
-                    <th scope="col">住所</th>
-                    <th scope="col">番地</th>
-                    <th scope="col">ビル名</th>
-                    <th scope="col">編集</th>
-                    <th scope="col">消去</th>
+                    <th class="py-3" scope="col">#</th>
+                    <th class="py-3" scope="col">法人名</th>
+                    <th class="py-3" scope="col">担当者氏名</th>
+                    <th class="py-3" scope="col">担当者ふりがな</th>
+                    <th class="py-3" scope="col">メールアドレス</th>
+                    <th class="py-3" scope="col">電話番号</th>
+                    <th class="py-3" scope="col">郵便番号</th>
+                    <th class="py-3" scope="col">住所</th>
+                    <th class="py-3" scope="col">番地</th>
+                    <th class="py-3" scope="col">ビル名</th>
+                    <th class="py-3" scope="col">編集</th>
+                    <th class="py-3" scope="col">消去</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <th scope="row">1</th>
-                    <td>a</td>
-                    <td>s</td>
-                    <td>s</td>
-                    <td>personal.codemaker@gmail.com</td>
-                    <td>123</td>
-                    <td>1111-1111111</td>
-                    <td>13</td>
-                    <td>e</td>
-                    <td>w</td>
-                    <td><button type="button" class="btn btn-outline-primary">編集</button></td>
-                    <td><button type="button" class="btn btn-outline-danger">消去</button></td>
+                    <th class="align-middle" scope="row">1</th>
+                    <td class="align-middle">a</td>
+                    <td class="align-middle">s</td>
+                    <td class="align-middle">s</td>
+                    <td class="align-middle">personal.codemaker@gmail.com</td>
+                    <td class="align-middle">123</td>
+                    <td class="align-middle">1111-1111111</td>
+                    <td class="align-middle">13</td>
+                    <td class="align-middle">e</td>
+                    <td class="align-middle">w</td>
+                    <td class="align-middle"><button type="button" class="btn btn-outline-primary">編集</button></td>
+                    <td class="align-middle"><button type="button" class="btn btn-outline-danger">消去</button></td>
                 </tr>
                 <tr>
                     <th scope="row">2</th>

15f2648989e7f7670d6dbc8b0bb22702cfb1dbb4
khdream <truedev1001@gmail.com>
2024-02-20 00:20:09 +0900

    https://github.com/khdream/member-management-Laravel-.git

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,7 +17,6 @@ class UserFactory extends Factory
     protected static ?string $password;
 
     /**
-     * Define the model's default state.
      *
      * @return array<string, mixed>
      */
